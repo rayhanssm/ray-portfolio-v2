56d53594230fd2b9763edb60782680e38d0b8f2a
Ray Mantiri <ray.mantiri@hashmicro.com.sg>
2026-04-20 13:43:19 +0700

    fix: update node

--- src/components/views/HomeView.tsx
+++ src/components/views/HomeView.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import HeroSection from "../sections/HeroSection";
-import SummarySection from "../sections/SummarySection";
-import WorksSection from "../sections/WorksSection";
 
 function HomeView() {
   return (
