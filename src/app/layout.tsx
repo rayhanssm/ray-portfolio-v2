e38e940e36c0dbd8a2374796939458b8ed1957b2
Ray Mantiri <ray.mantiri@hashmicro.com.sg>
2026-04-27 01:07:26 +0700

    feat: add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,15 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
+import AnalyticsTracker from "@/components/AnalyticsTracker";
 
 export const metadata: Metadata = {
   title: "Ray Mantiri",
   description: "Ray's personal portfolio website",
 };
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({
   children,
@@ -15,7 +19,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Load GA script */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        {/* Init GA */}
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
+
       <body>
+        <AnalyticsTracker />
         <Navbar />
         {children}
         <Footer />
---
+++ src/components/AnalyticsTracker.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+export default function AnalyticsTracker() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.gtag?.("config", process.env.NEXT_PUBLIC_GA_ID, {
+      page_path: pathname,
+    });
+  }, [pathname]);
+
+  return null;
+}
